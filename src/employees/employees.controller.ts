e685c6822bedf2bb5e6309f882862e767e08518a
Victor Vitalino <victorvitalino237@gmail.com>
2024-10-22 17:14:16 -0300

    :construction: feat: add exceptions for rate limit in some routes

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -10,7 +10,9 @@ import {
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
+import { SkipThrottle, Throttle } from '@nestjs/throttler';
 
+@SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
@@ -20,11 +22,13 @@ export class EmployeesController {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  @SkipThrottle({ default: false }) // overides default conf
   @Get()
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
     return this.employeesService.findAll(role);
   }
 
+  @Throttle({ short: { ttl: 1000, limit: 1 } }) // overides configuration for short
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(+id);
